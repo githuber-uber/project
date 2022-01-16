3c2301da72ab55ef9b877a027f6d353ab19a6459
anon <viktor.ejow@yandex.ru>
2022-01-16 14:09:09 +0300

    first tech. assignment complete

--- js/script.js
+++ js/script.js
@@ -48,22 +48,23 @@
 // const user = "Ivan";
 // alert(`Привет, ${user}`);
 
-console.log('arr' + " - object");
-console.log(4 + +"5");
+// console.log('arr' + " - object");
+// console.log(4 + +"5");
 
-let incr = 10,
-    decr = 10;
+// let incr = 10,
+//     decr = 10;
 
-// ++incr;
-// --decr;
+// // ++incr;
+// // --decr;
 
-console.log(++incr);
-console.log(--decr);
+// console.log(++incr);
+// console.log(--decr);
 
-console.log(5%2);
-console.log(2 + 2 * 2 !== '6');
+// console.log(5%2);
+// console.log(2 + 2 * 2 !== '6');
 
 
-const isChecked = false,
-      isClose = false;
-console.log(isChecked || !isClose);
+// const isChecked = false,
+//       isClose = false;
+// console.log(isChecked || !isClose);
+
