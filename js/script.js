5bd1113ca6872c3f8f9844667ccde74e5a6bf747
anon <viktor.ejow@yandex.ru>
2022-02-05 21:25:22 +0300

    third tech. assignment complete

--- js/script.js
+++ js/script.js
@@ -102,7 +102,7 @@
 //         console.log('Не в этот раз');
 // }
 
-let num = 50;
+// let num = 50;
 
 // while (num <= 55) {
 //     console.log(num);
@@ -115,10 +115,80 @@ let num = 50;
 // }
 // while (num < 55);
 
-for (let i = 1; i < 10; i++) {
-    if (i === 6) {
-        // break;
-        continue;
-    }
-    console.log(i);
-}
+// for (let i = 1; i < 10; i++) {
+//     if (i === 6) {
+//         // break;
+//         continue;
+//     }
+//     console.log(i);
+// }
+// let num = 20;
+
+// function showFirstMessage(text) {
+//     console.log(text);
+//     console.log(num);
+// }
+
+// showFirstMessage("Hello World!");
+// console.log(num);
+
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(10, 6));
+
+// function calc(a, b) {
+//     return (a +  b);
+// }
+
+// function ret() {
+//     let num = 50;
+// 
+    
+// 
+// 
+//     return num;
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+// const logger = function() {
+//     console.log("Hello");
+// };
+
+// logger();
+
+// const calc = (a, b) => {
+//     console.log('1');
+//     return(a + b);
+// };
+
+const str = 'teSt';
+const arr = [1, 2, 4];
+
+// console.log(str[2] = 'd');
+
+// console.log(str.toUpperCase());
+console.log(str.toLocaleLowerCase());
+console.log(str);
+
+const fruit = "Some fruit";
+
+console.log(fruit.indexOf("q"));
+
+const logg = "Hello world";
+
+// console.log(logg.slice(6, 11));
+
+// console.log(logg.substring(6, 11));
+
+console.log(logg.substr(6, 5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+// console.log(parseInt(test));
+console.log(parseFloat(test));
+
+
